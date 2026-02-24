c6ded2c110f37e206ee623f35fdbd1f2c226129b
ArTeGaS-2 <probablytorvik@gmail.com>
2026-02-24 13:42:21 +0200

    Доопрацювали камеру: додали керування мишею (X/Y) з фіксацією по Z та оновили сцену RedBall.

--- Assets/RedBalls/CameraControl.cs
+++ Assets/RedBalls/CameraControl.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    [SerializeField] private GameObject cameraObj;
+    private float mouseVectorX = 0f;
+    private float mouseVectorY = 0f;
+
+    [SerializeField] private bool staticCamera = false;
+    private void FixedUpdate()
+    {
+        mouseVectorX = Input.GetAxis("Mouse X");
+        mouseVectorY = Input.GetAxis("Mouse Y");
+
+        if (!staticCamera) 
+        {
+            cameraObj.transform.Rotate(new Vector3(
+            -mouseVectorY, mouseVectorX, 0f));
+
+            Vector3 eulerAngles = cameraObj.transform.eulerAngles;
+            eulerAngles.z = 0f;
+            cameraObj.transform.eulerAngles = eulerAngles;
+        }
+    }
     private void LateUpdate()
     {
-        transform.position = new Vector3(
+        cameraObj.transform.position = new Vector3(
             transform.position.x,
             transform.position.y + 5f,
             transform.position.z - 10f);
