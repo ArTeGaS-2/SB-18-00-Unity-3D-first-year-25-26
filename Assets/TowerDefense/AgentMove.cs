9fb82fd1aef56173a965bab7749d0aba75d190a9
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-17 12:21:04 +0200

    Продовжили роботу над TowerDefense створивши ворога з базовою навігацією

--- Assets/TowerDefense/AgentMove.cs
+++ Assets/TowerDefense/AgentMove.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentMove : MonoBehaviour
 {
+    // назва об'єкта, до якого буде рухатися агент
+    [SerializeField] string targetName = "EnemyTarget";
+    // максимальний рівень здоров'я агента
+    [SerializeField] float maxHp = 20f;
 
+    // Посилання на NavMeshAgent для керування рухом агента
+    private NavMeshAgent agent;
+    // Поточний рівень здоров'я агента
+    private float currentHp;
+
+    private void Start()
+    {
+        // Ініціалізація поточного здоров'я агента
+        currentHp = maxHp;
+        // Отримання компонента NavMeshAgent
+        agent = GetComponent<NavMeshAgent>();
+        // Встановлення цілі для руху агента
+        agent.SetDestination(GameObject.Find(
+            targetName).transform.position);
+    }
 }
