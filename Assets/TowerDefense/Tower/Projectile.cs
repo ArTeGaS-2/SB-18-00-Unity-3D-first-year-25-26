ef10b63ea439c20c38bedd5df9b5e40461403384
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-29 10:33:47 +0300

    Додали базовий проджектайл і ціль

---
+++ Assets/TowerDefense/Tower/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject targetObj; // ціль кулі
+    [SerializeField] private float projectileSpeed = 10f;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetObj.transform.position,
+            projectileSpeed * Time.fixedDeltaTime);
+
+        transform.LookAt(targetObj.transform);
+    }
+
+
+}
