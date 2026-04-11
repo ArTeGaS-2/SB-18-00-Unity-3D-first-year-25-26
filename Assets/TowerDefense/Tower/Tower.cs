329adf7d7a41073e3c99c2704307c431f90b3654
ArTeGaS-2 <probablytorvik@gmail.com>
2026-04-11 21:33:38 +0300

    Додали механіку спавну проджектайлів

--- Assets/TowerDefense/Tower/Tower.cs
+++ Assets/TowerDefense/Tower/Tower.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    
+    [Header("Проджектайл")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject spawnPoint;
+    [SerializeField] float attackInterval;
+
+    [Header("Інше")]
+    [HideInInspector] public List<GameObject> enemiesList;
+
+    private void Start()
+    {
+        StartCoroutine(ProjectileSpawn());
+    }
+
+    private IEnumerator ProjectileSpawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(attackInterval);
+            Instantiate(
+                projectilePrefab,
+                spawnPoint.transform.position,
+                Quaternion.identity
+                );
+        }
+    }
 }
